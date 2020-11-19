a5fd0ef8b6b6bb239bd9f587dd9aece7d972ab17
leon.zhou <zhouleib1412@gmail.com>
2020-11-19 16:32:51 +1300

    bump up version

--- user-api/build.gradle.kts
+++ user-api/build.gradle.kts
@@ -24,7 +24,7 @@ noArg {
 }
 
 group = "com.github.b1412"
-version = "0.1.13"
+version = "0.1.14"
 
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
--- user-generator/build.gradle.kts
+++ user-generator/build.gradle.kts
@@ -15,7 +15,7 @@ val bootJar: BootJar by tasks
 bootJar.enabled = false
 jar.enabled = true
 
-version = "0.1.13"
+version = "0.1.14"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
