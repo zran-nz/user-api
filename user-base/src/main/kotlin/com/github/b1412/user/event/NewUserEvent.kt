4755ecb2fa1539e0645de9be98b8de0de7836eb8
Leon Zhou <zhouleib1412@gmail.com>
2020-12-17 16:59:13 +1300

    0.1.27

--- user-base/src/main/kotlin/com/github/b1412/user/event/NewUserEvent.kt
+++ user-base/src/main/kotlin/com/github/b1412/user/event/NewUserEvent.kt
@@ -2,4 +2,4 @@ package com.github.b1412.user.event
 
 import org.springframework.context.ApplicationEvent
 
-class NewUserEvent(source: Map<String, String>, val action: String) : ApplicationEvent(source)
+data class NewUserEvent(val source: Map<String, String>, val action: String) : ApplicationEvent(source)
